ea7a684a549386849cf69b08ecf7596d1ee7731e
Fujimoto Seiji <fujimoto@clear-code.com>
2022-10-30 07:24:46 +0000

    Add a new interface 'get_utterance()'
    This replaces the older function 'align_audio()' and significantly
    simplifies the call interface:
    
        get_utterance("test.m2ts", ctc_segmentation)
    
    Signed-off-by: Fujimoto Seiji <fujimoto@clear-code.com>

--- reazonspeech/align.py
+++ reazonspeech/align.py
@@ -1,6 +1,9 @@
+from .utils import load_audio
+from .caption import get_captions
+from .sentence import build_sentences
 from .interface import Utterance
 
-__all__ = "align_audio",
+__all__ = "get_utterances",
 
 _MARGIN = (25.0, 3.0)
 _PADDING = (0.0, 0.1)
@@ -10,43 +13,46 @@ def _slice(buffer, samplerate, start, end):
     end = min(int(end * samplerate), len(buffer))
     return buffer[start:end]
 
-def align_audio(buffer, samplerate, captions, ctc_segmentation,
-                margin=_MARGIN, padding=_PADDING):
-    """Get utterances corresponding to captions.
+def _align(buffer, samplerate, caption, ctc_segmentation):
+    source = _slice(buffer, samplerate,
+                    caption.start_seconds - _MARGIN[0],
+                    caption.end_seconds + _MARGIN[1])
+
+    try:
+        aligned = ctc_segmentation(source, caption.text)
+    except (IndexError, ValueError):
+        return None
+
+    if aligned.segments:
+        start, end, score = aligned.segments[0]
+        start -= _PADDING[0]
+        end += _PADDING[1]
+        duration = (end - start)
+
+        return Utterance(_slice(source, samplerate, start, end),
+                         samplerate=samplerate,
+                         duration=duration,
+                         text=caption.text,
+                         score=score)
+    return None
+
+def get_utterances(path, ctc_segmentation):
+    """Extract utterances from MPEG-TS data
 
     Args:
-        buffer (numpy.array): The audio wave data.
-        samplerate (int): The sample rate of the audio data.
-        captions (list of Caption): The captions to align audio data.
-        ctc_segmentation (espnet2.bin.asr_align.CTCSegmentation): The alignment algorithm.
-        margin (tuple of int): Add N seconds to segments (pre-alignment)
-        padding (tuple of int): Add N seconds to segments (post-alignment)
+      path (str): Path to a M2TS file
+      ctc_segmentation (espnet2.bin.asr_align.CTCSegmentation): An audio aligner
 
     Returns:
-        A list of `Utterance` instances.
+      A list of Utterance objects
     """
+    samplerate = int(ctc_segmentation.fs)
+    captions = build_sentences(get_captions(path))
+    buffer = load_audio(path, samplerate)
 
     utterances = []
     for caption in captions:
-        source = _slice(buffer, samplerate,
-                        caption.start_seconds - margin[0],
-                        caption.end_seconds + margin[1])
-
-        try:
-            aligned = ctc_segmentation(source, caption.text)
-        except (IndexError, ValueError):
-            continue
-
-        if aligned.segments:
-            start, end, score = aligned.segments[0]
-            start -= padding[0]
-            end += padding[1]
-            duration = (end - start) / samplerate
-
-            utt = Utterance(_slice(source, samplerate, start, end),
-                            samplerate=samplerate,
-                            duration=duration,
-                            text=caption.text,
-                            score=score)
+        utt = _align(buffer, samplerate, caption, ctc_segmentation)
+        if utt:
             utterances.append(utt)
     return utterances
