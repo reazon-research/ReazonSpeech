cd32a043ac42cf830d7d7561a6b280f1b0196a91
yuta0306 <yuta.20010306@gmail.com>
2026-01-19 22:53:17 +0900

    fix: export some shared functions from pkg __init__

--- pkg/k2-asr/src/reazonspeech/k2/asr/__init__.py
+++ pkg/k2-asr/src/reazonspeech/k2/asr/__init__.py
@@ -1,4 +1,18 @@
+from reazonspeech.shared.audio import (
+    audio_from_numpy,
+    audio_from_path,
+    audio_from_tensor,
+)
+from reazonspeech.shared.interface import TranscribeConfig
+
 from .huggingface import load_model
 from .transcribe import transcribe
 
-__all__ = ["load_model", "transcribe"]
+__all__ = [
+    "TranscribeConfig",
+    "audio_from_numpy",
+    "audio_from_path",
+    "audio_from_tensor",
+    "load_model",
+    "transcribe",
+]
--- pkg/nemo-asr/src/reazonspeech/nemo/asr/__init__.py
+++ pkg/nemo-asr/src/reazonspeech/nemo/asr/__init__.py
@@ -1,3 +1,17 @@
-from .interface import TranscribeConfig
-from .transcribe import transcribe, load_model
-from .audio import audio_from_numpy, audio_from_tensor, audio_from_path
+from reazonspeech.shared.audio import (
+    audio_from_numpy,
+    audio_from_path,
+    audio_from_tensor,
+)
+from reazonspeech.shared.interface import TranscribeConfig
+
+from .transcribe import load_model, transcribe
+
+__all__ = [
+    "TranscribeConfig",
+    "audio_from_numpy",
+    "audio_from_path",
+    "audio_from_tensor",
+    "load_model",
+    "transcribe",
+]
